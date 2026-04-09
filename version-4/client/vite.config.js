0a1103c468caab601fe6c9ce844341dee3c54ebf
jennivonsta <jencvon@gmail.com>
2026-04-09 15:07:10 -0400

    connected frontend to backend

--- version-4/client/vite.config.js
+++ version-4/client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000", // ✅ now points to backend
+        target: "https://countries-app-api-server-jenny.onrender.com", // ✅ now points to backend -key value (localhost 3000, a string) pair
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
